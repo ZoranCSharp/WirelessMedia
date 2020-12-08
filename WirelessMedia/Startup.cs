11f757a020b9d15e21a1437173b27924b56c9d95
Zoran Kovacevic <zorank.java@gmail.com>
2020-12-08 15:31:05 +0100

    ADDED - Areas Customer, Admin

--- WirelessMedia/Startup.cs
+++ WirelessMedia/Startup.cs
@@ -64,7 +64,7 @@ namespace WirelessMedia
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
