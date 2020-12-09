8b59523bcca19a070dd276eb3225c9c01abab7a7
Zoran Kovacevic <zorank.java@gmail.com>
2020-12-09 15:58:11 +0100

    ADDED - Index View for Product

--- WirelessMedia/Areas/Admin/Controllers/ProductController.cs
+++ WirelessMedia/Areas/Admin/Controllers/ProductController.cs
@@ -1,17 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WirelessMedia.Data;
 
 namespace WirelessMedia.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
-        public IActionResult Index()
+
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
         }
+
+        //GET - ACTION METHOD
+        public async Task<IActionResult> Index()
+        {
+            var productList = await _db.Product.Include(z => z.Category).ToListAsync();
+
+            return View(productList);
+        }
+
+        
     }
 }
